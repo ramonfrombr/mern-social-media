b5ccc300a7898f689a5527b0ae4377dbf7152179
Ramon Rodrigues <ramonfrombr@gmail.com>
2025-11-01 14:32:53 -0300

    Initialized Node packages. Initialized MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
-
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((error) => console.error("Error trying to connect:", error));
 
 app.listen(8800, () => {
     console.log("Backend server is running!!!");
