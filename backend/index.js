e79d693bb25be97766fc1edeec868f300d2e51de
Ramon Rodrigues <ramonfrombr@gmail.com>
2025-11-01 15:35:09 -0300

    Implemented simple get route

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,15 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
+app.get("/", (request, response) => {
+    response.send("Welcome to my website");
+});
+
+app.get("/users", (request, response) => {
+    response.send("Welcome to my website users page");
+});
+
+
 app.listen(8800, () => {
     console.log("Backend server is running!!!");
 });
