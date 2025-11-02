c039b51f3ad384c2992e70c24f01d68ab5ac0eac
Ramon Rodrigues <ramonfrombr@gmail.com>
2025-11-02 14:16:01 -0300

    Implemented password hashing

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,22 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 // REGISTER
 router.post("/register", async (request, response) => {
-    const newUser = new User({
-        username: request.body.username,
-        email: request.body.email,
-        password: request.body.password
-    });
-
     try {
+        // Generate hashed password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(request.body.password, salt);
+
+        // Create user
+        const newUser = new User({
+            username: request.body.username,
+            email: request.body.email,
+            password: hashedPassword
+        });
+
+        // Save user and respond
         const user = await newUser.save();
         response.status(200).json(user);
     } catch (error) {
