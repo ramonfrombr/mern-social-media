f6d8d52baba5e71e0de2bafa73b267ce4ab61d63
Ramon Rodrigues <ramonfrombr@gmail.com>
2025-11-02 14:09:56 -0300

    Implemented post route to register user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,15 +2,19 @@ const router = require("express").Router();
 const User = require("../models/User");
 
 // REGISTER
-router.get("/register", async (request, response) => {
-    const user = await new User({
-        username: "john",
-        email: "john@gmail.com",
-        password: "123456",
+router.post("/register", async (request, response) => {
+    const newUser = new User({
+        username: request.body.username,
+        email: request.body.email,
+        password: request.body.password
     });
 
-    await user.save();
-    response.send("ok");
+    try {
+        const user = await newUser.save();
+        response.status(200).json(user);
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 module.exports = router;
