8b06b1163c41e55ddd095d5085766f98cc62bbba
Ramon Rodrigues <ramonfrombr@gmail.com>
2025-11-02 15:27:16 -0300

    Implemented update user route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,37 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
 
-router.get("/", (request, response) => {
-    response.send("This is the users route.");
+// update user
+router.put("/:id", async (request, response) => {
+    if (request.body.userId === request.params.id || request.user.isAdmin) {
+        if (request.body.password) {
+            try {
+                const salt = await bcrypt.genSalt(10);
+                request.body.password = await bcrypt.hash(
+                    request.body.password,
+                    salt
+                );
+            } catch (error) {
+                return response.status(500).json(error);
+            }
+        }
+
+        try {
+            const user = await User.findByIdAndUpdate(request.params.id, {
+                $set: request.body,
+            });
+            response.status(200).json("Account has been updated.");
+        } catch (error) {
+            response.status(500).json(error);
+        }
+    } else {
+        return response.status(403).json("You can only updaet your account.");
+    }
 });
+// delete user
+// get user
+// follow a user
+// unfollow a user
 
 module.exports = router;
