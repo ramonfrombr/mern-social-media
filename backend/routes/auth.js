a47ccb9c9c886a277d2310807a7a4a365979a425
Ramon Rodrigues <ramonfrombr@gmail.com>
2025-11-02 14:26:23 -0300

    Implemented login route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -13,14 +13,31 @@ router.post("/register", async (request, response) => {
         const newUser = new User({
             username: request.body.username,
             email: request.body.email,
-            password: hashedPassword
+            password: hashedPassword,
         });
 
         // Save user and respond
         const user = await newUser.save();
         response.status(200).json(user);
     } catch (error) {
-        console.log(error);
+        response.status(500).json(error);
+    }
+});
+
+router.post("/login", async (request, response) => {
+    try {
+        const user = await User.findOne({ email: request.body.email });
+        !user && response.status(404).send("User not found");
+
+        const validPassword = await bcrypt.compare(
+            request.body.password,
+            user.password
+        );
+        !validPassword && response.status(400).send("Incorrect password");
+
+        response.status(200).json(user);
+    } catch (error) {
+        response.status(500).json(error);
     }
 });
 
